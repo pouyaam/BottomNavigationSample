e87d235ca023cec13d790e3bdb48fb284d751601
Pouya Amirahmadi <amirahmadipouya@gmail.com>
2020-10-05 22:10:49 +0330

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:bottom_nav_bar_sample/sample_bottom_navbar_controller.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Bottom Nav Bar Sample',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: SampleBottomNavBarController(),
+    );
+  }
+}
